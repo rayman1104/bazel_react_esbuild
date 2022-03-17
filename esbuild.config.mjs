03907ada30a2e85e04a63c3021fd3417c8f58f17
rayman1104 <rayman1104@gmail.com>
2022-03-17 15:23:24 +0300

    css works but not as expected

---
+++ esbuild.config.mjs
@@ -0,0 +1,19 @@
+import { default as cssModulesPlugin } from "esbuild-css-modules-plugin";
+
+
+export default {
+    plugins: [
+        cssModulesPlugin({
+            // optional. set to false to not inject generated CSS into <head>, default is true.
+            // could be a function with params content & digest (return a string of js code to inject to page),
+            // e.g.
+            // ```
+            // inject: (cssContent, digest) => `console.log("${cssContent}", "${digest}")`
+            // ```
+            inject: true,
+
+            localsConvention: 'camelCaseOnly', // optional. value could be one of 'camelCaseOnly', 'camelCase', 'dashes', 'dashesOnly', default is 'camelCaseOnly'
+            v2: true,
+        }),
+    ],
+}
--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import * as styles from "./styles.css";
 
 ReactDOM.render(
-  <h1 className={styles.h1}>Hello, world!</h1>,
+  <h1 className="h1">Hello, world!</h1>, // works
+  // <h1 className={styles.h1}>Hello, world!</h1>, // doesn't work
   document.getElementById("root")
 );
