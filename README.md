986f195fdbe81cb32138bc9549e46effe78bf0a0
rayman1104 <rayman1104@gmail.com>
2022-03-28 16:09:31 +0300

    add antd

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
+## Description
+
 This PoC shows how to use Esbuild to build and serve a React app with Bazel.
 
 Bazel takes care of things like Sass and TypeScript compilation before calling esbuild.
 
 CSS Modules don't work as expected right now. See issue https://github.com/evanw/esbuild/issues/20.
+
+## How to run
+
+```bash
+$ bazel run //:server
+```
+
+## CSS Modules issue
 
 Works:
 ```tsx
--- index.tsx
+++ index.tsx
@@ -1,9 +1,18 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as styles from "./styles.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button, DatePicker } from 'antd';
+
+import 'antd/dist/antd.css';
+import './styles.css';
 
 ReactDOM.render(
-  <h1 className="h1">Hello, world!</h1>, // works
-  // <h1 className={styles.h1}>Hello, world!</h1>, // doesn't work
+  <div className="App">
+    <h1 className="h1">Hello, world!</h1>
+    {/*<h1 className={styles.h1}>Hello, world!</h1>, // doesn't work*/}
+    <DatePicker />
+    <Button type="primary" style={{ marginLeft: 8 }}>
+      Primary Button
+    </Button>
+  </div>,
   document.getElementById("root")
 );
